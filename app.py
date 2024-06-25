ff2b8d0fe0e752f91b426ab7b6d3be862402f181
Tansu Baktiran <tansu@baktiran.com>
2024-06-25 14:37:48 +0300

    some updates

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
