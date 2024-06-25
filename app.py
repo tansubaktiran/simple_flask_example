a72c78b18418d1df9e0832eb7d586e2f49c0c9da
Tansu Baktiran <tansu@baktiran.com>
2024-06-25 14:44:20 +0300

    new branch change

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def home():
 def contact():
     return render_template('contact.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
